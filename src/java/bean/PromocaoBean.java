d472d91f52fe13bc1b0edc7f476304e7b5323d6b
Unknown <igorozani@gmail.com>
2019-05-27 11:33:38 -0300

    Teste
    Commit de teste

--- src/java/bean/PromocaoBean.java
+++ src/java/bean/PromocaoBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bean;
+package bean; //Igor
 
 import dao.PromocaoDAO;
 import dao.TeatroDAO;
