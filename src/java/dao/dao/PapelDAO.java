b2ce6cebbf9e9594227cff57d9ea0f7c5ef1d511
Igor José Lima Rozani <33789352+igorozani@users.noreply.github.com>
2019-05-31 00:50:08 -0300

    Adicionando login

---
+++ src/java/dao/dao/PapelDAO.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import pojo.Papel;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Igor
+ */
+public class PapelDAO extends GenericDAO<Papel>{
+    
+    @Override
+    public void save(Papel papel) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(papel);
+        tx.commit();
+        em.close();
+    }
+    
+    @Override
+    public List<Papel> getAll() {
+        EntityManager em = this.getEntityManager();
+        Query q = em.createQuery("select p from Papel p", Papel.class);
+        List<Papel> papeis = q.getResultList();
+        em.close();
+        return papeis;
+    }
+
+    @Override
+    public void delete(Papel papel) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        papel = em.getReference(Papel.class, papel.getId());
+        tx.begin();
+        em.remove(papel);
+        tx.commit();
+    }
+    
+    @Override
+    public void update(Papel papel) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(papel);
+        tx.commit();
+        em.close();
+    }
+
+    @Override
+    public Papel get(Integer id) {
+        EntityManager em = this.getEntityManager();
+        Papel papel = em.find(Papel.class, id);
+        em.close();
+        return papel;
+    }
+}
+
